283d6d8fc354b4ec3838fb470e8ca1ec39924092
jericco1 <jericcojohn@gmail.com>
2023-04-19 20:59:16 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+score = int(input('Enter Your score: '))
+
+if score > 100 or score < 0:
+    print("Not Allowed")
+elif score> 69:
+    print("A")
+elif score > 59 and score < 70:
+    print('B')
+elif score > 49 and score < 60:
+    print('C')
+elif score > 39 and score < 50:
+    print('D')
+elif score > 29 and score < 40:
+    print('E')
+else:
+    print('F')
+    
+
